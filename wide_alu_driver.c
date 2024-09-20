65b5f21fc9c0ef0101a94f8c81f1726b1d77fbd4
Renato Belmonte <73286141+RenatoBelmonte@users.noreply.github.com>
2024-09-20 15:50:46 +0200

    TRY NEW REGISTER FORMAT

--- wide_alu_driver.c
+++ wide_alu_driver.c
@@ -1,29 +1,32 @@
 #include <wide_alu_driver.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "wide_alu.h"
+
+#define WIDE_ALU0_BASE_ADDR 0x1A400000
 
 void set_op(uint8_t operation)
 {
-    uint32_t volatile *ctrl2_reg = (uint32_t *)WIDE_ALU_CTRL2(0);
+    uint32_t volatile *ctrl2_reg = (uint32_t *)(WIDE_ALU_CTRL2_REG_OFFSET + WIDE_ALU0_BASE_ADDR);
     uint32_t ctrl2_old_value = *ctrl2_reg;
-    *ctrl2_reg = ctrl2_old_value | (operation & WIDE_ALU_CTRL2_OPSEL_MASK) << WIDE_ALU_CTRL2_OPSEL_LSB;
+    *ctrl2_reg = ctrl2_old_value | (operation & WIDE_ALU_CTRL2_OPSEL_MASK);
 }
 
 void set_delay(uint8_t delay)
 {
-  uint32_t volatile * ctrl2_reg = (uint32_t*)WIDE_ALU_CTRL2(0);
+  uint32_t volatile * ctrl2_reg = (uint32_t*)(WIDE_ALU_CTRL2_REG_OFFSET + WIDE_ALU0_BASE_ADDR);
   uint32_t ctrl2_old_value;
   //Read old value
   ctrl2_old_value = *ctrl2_reg;
   //Overwrite operation bits
-  *ctrl2_reg = ctrl2_old_value | (delay & WIDE_ALU_CTRL2_DELAY_MASK)<<WIDE_ALU_CTRL2_DELAY_LSB;
+  *ctrl2_reg = ctrl2_old_value | (delay & WIDE_ALU_CTRL2_DELAY_MASK);
 }
 
 
 void set_operands(uint32_t* a, uint32_t* b)
 {
-  uint32_t volatile * op_a_reg_start = (uint32_t*)WIDE_ALU_OP_A_0(0);
-  uint32_t volatile * op_b_reg_start = (uint32_t*)WIDE_ALU_OP_B_0(0);
+  uint32_t volatile * op_a_reg_start = (uint32_t*)WIDE_ALU_OP_A_0_REG_OFFSET;
+  uint32_t volatile * op_b_reg_start = (uint32_t*)WIDE_ALU_OP_B_0_REG_OFFSET;
   //Make sure we are in idle state before changing the operands
   for (int i = 0; i<256/32; i++)
   {
@@ -33,20 +36,20 @@ void set_operands(uint32_t* a, uint32_t* b)
 }
 
 void trigger_op(void) {
-    uint32_t volatile *ctrl1_reg = (uint32_t *)WIDE_ALU_CTRL1(0);
+    uint32_t volatile *ctrl1_reg = (uint32_t *)(WIDE_ALU_CTRL1_REG_OFFSET + WIDE_ALU0_BASE_ADDR);
     asm volatile ("": : : "memory");
-    *ctrl1_reg = (1 & WIDE_ALU_CTRL1_TRIGGER_MASK) << WIDE_ALU_CTRL1_TRIGGER_LSB;
+    *ctrl1_reg = (1 & 0x1) << WIDE_ALU_CTRL1_TRIGGER_BIT;
     asm volatile ("": : : "memory");
 }
 
 int poll_done(void)
 {
-  uint32_t volatile * status_reg = (uint32_t*)WIDE_ALU_STATUS(0);
+  uint32_t volatile * status_reg = (uint32_t*)(WIDE_ALU_STATUS_REG_OFFSET + WIDE_ALU0_BASE_ADDR);
   uint32_t current_status;
   do {
-    current_status = (*status_reg>>WIDE_ALU_STATUS_CODE_LSB)&WIDE_ALU_STATUS_CODE_MASK;
-  } while(current_status == WIDE_ALU_STATUS_CODE_PENDING);
-  if (current_status == WIDE_ALU_STATUS_CODE_IDLE)
+    current_status = (*status_reg)&WIDE_ALU_STATUS_CODE_MASK;
+  } while(current_status == WIDE_ALU_STATUS_CODE_VALUE_PENDING);
+  if (current_status == WIDE_ALU_STATUS_CODE_VALUE_IDLE)
     return 0;
   else
     return current_status;
@@ -54,7 +57,7 @@ int poll_done(void)
 
 void get_result(uint32_t* result)
 {
-  uint32_t volatile * result_reg_start = (uint32_t*)WIDE_ALU_RESULT_0(0);
+  uint32_t volatile * result_reg_start = (uint32_t*)(WIDE_ALU_RESULT_0_REG_OFFSET + WIDE_ALU0_BASE_ADDR);
   for (int i = 0; i<512/32; i++)
   {
     result[i] = result_reg_start[i];
@@ -63,9 +66,9 @@ void get_result(uint32_t* result)
 
 void clear_error(void)
 {
-  uint32_t volatile * ctrl1_reg = (uint32_t*)WIDE_ALU_CTRL1(0);
+  uint32_t volatile * ctrl1_reg = (uint32_t*)(WIDE_ALU_CTRL1_REG_OFFSET + WIDE_ALU0_BASE_ADDR);
   //Trigger operation by writing to trigger bit
-  *ctrl1_reg = (1 & WIDE_ALU_CTRL1_CLEAR_ERR_MASK)<<WIDE_ALU_CTRL1_CLEAR_ERR_LSB;
+  *ctrl1_reg = (1 & 0x1)<<WIDE_ALU_CTRL1_CLEAR_ERR_BIT;
 }
 
 int wide_multiply(uint32_t a[8], uint32_t b[8], uint32_t result[16]) {
@@ -75,7 +78,7 @@ int wide_multiply(uint32_t a[8], uint32_t b[8], uint32_t result[16]) {
 	return status;
     }
     set_operands(a, b);
-    set_op(WIDE_ALU_CTRL2_OPSEL_MUL);
+    set_op(WIDE_ALU_CTRL2_OPSEL_VALUE_MUL);
 
     trigger_op();
 
